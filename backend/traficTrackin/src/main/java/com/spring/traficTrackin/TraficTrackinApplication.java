620ce1bb47565007e36eb126eb8a1901fab59ddb
kique <enrherbay@alu.edu.gva.es>
2025-01-28 16:15:07 +0100

    primer commit, y estoy hasta la p ya

---
+++ backend/traficTrackin/src/main/java/com/spring/traficTrackin/TraficTrackinApplication.java
@@ -0,0 +1,13 @@
+package com.spring.traficTrackin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficTrackinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficTrackinApplication.class, args);
+	}
+
+}
