1b58895bc4587fd8443f79af82137f8229c984dc
Enrique Clase <soyenriqueh@hotmail.com>
2025-03-07 15:46:24 +0100

    cambios front

--- backend/database/creadbTrafficTrackin.sql
+++ backend/database/creadbTrafficTrackin.sql
@@ -1,9 +1,10 @@
--- este script crea una base de datos simple con una tabla usuarios (id pk, email nn unique, password nn)
+-- este script crea una base de datos simple con una tabla usuarios (id pk, email nn unique, userName nn unique, password nn)
 CREATE DATABASE IF NOT EXISTS dbTrafficTracking;
 USE dbTrafficTracking;
 
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
+    userName VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
