ed3c38fa787baece58a795ec1edd40727a880145
Enrique Clase <soyenriqueh@hotmail.com>
2025-03-11 16:58:11 +0100

    a

--- backend/database/creadbTrafficTrackin.sql
+++ backend/database/creadbTrafficTrackin.sql
@@ -5,7 +5,7 @@ USE dbTrafficTracking;
 
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-        user_name VARCHAR(30) NOT NULL UNIQUE,
+    user_name VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
