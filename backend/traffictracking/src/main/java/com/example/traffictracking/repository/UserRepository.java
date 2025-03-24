723d2a81ade5763e1f39734021eff8bd4a05d07e
Enrique Clase <soyenriqueh@hotmail.com>
2025-03-24 20:26:27 +0100

    find by email

--- backend/traffictracking/src/main/java/com/example/traffictracking/repository/UserRepository.java
+++ backend/traffictracking/src/main/java/com/example/traffictracking/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.traffictracking.repository;
 
 
 import com.example.traffictracking.model.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
