e0d4b282858fee638f2104b5eea17a1f255cc81c
Enrique Clase <soyenriqueh@hotmail.com>
2025-04-15 17:29:50 +0200

    correccion controllers

---
+++ backend/traffictracking/src/main/java/com/example/traffictracking/controller/TrafficParkingsController.java
@@ -0,0 +1,13 @@
+package com.example.traffictracking.controller;
+import com.example.traffictracking.service.TrafficService;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/data")
+public class TrafficParkingsController {
+
+}
