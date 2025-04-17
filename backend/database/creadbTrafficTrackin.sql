3c6fc2e8928ba9a27044aed203e7202715e1e467
Enrique <soyenriqueh@hotmail.com>
2025-04-17 22:10:48 +0200

    cambios script sql bd

--- backend/database/creadbTrafficTrackin.sql
+++ backend/database/creadbTrafficTrackin.sql
@@ -1,13 +1,13 @@
--- este script crea una base de datos simple con una tabla usuarios (id, email, userName, password)
 
- CREATE DATABASE IF NOT EXISTS dbTrafficTracking;
+CREATE DATABASE IF NOT EXISTS dbTrafficTracking;
 USE dbTrafficTracking;
 
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    fechaCreado TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE lugares_favoritos (
@@ -16,5 +16,6 @@ CREATE TABLE lugares_favoritos (
     nombre VARCHAR(150) NOT NULL,
     latitud DECIMAL(9,6),
     longitud DECIMAL(9,6),
+    fechaCreado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
