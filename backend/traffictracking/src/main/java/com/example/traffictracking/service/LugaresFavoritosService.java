9828a9c5bf2b7e571cab300d3ece28013e8038a7
Enrique Clase <soyenriqueh@hotmail.com>
2025-05-07 17:25:35 +0200

    remove unnecessary services

--- backend/traffictracking/src/main/java/com/example/traffictracking/service/LugaresFavoritosService.java
+++
@@ -1,8 +0,0 @@
-package com.example.traffictracking.service;
-import org.springframework.stereotype.Service;
-//aqui iria logica de lugares favoritos pero se ha hecho en el controlador directamente
-@Service
-public class LugaresFavoritosService {
-
-   
-}
--- backend/traffictracking/src/main/java/com/example/traffictracking/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.example.traffictracking.service;
-//aqui iria logica de usuarios pero se ha hecho en el controlador directamente
-public class UserService {
-    
-}
